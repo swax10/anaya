77fd177c1da829ca0881a8a9ca2749a2570ba6f1
Shreyas Waghnode <shreyasw496@gmail.com>
2024-07-04 15:11:43 +0530

    chore: Update python and dependencies versions

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from anaya.core import Anaya
+import streamlit as st
 
 if __name__ == "__main__":
+    title = "Anaya🔥📑"
+    st.set_page_config(
+            page_title=title,
+            page_icon="🔥",
+            layout="wide",
+            initial_sidebar_state="collapsed",
+        )
     anaya = Anaya(
-            title="Anaya🔥📑",
+            title=title,
             initial_message="Hello! I am **Anaya🔥**. How can I help you today?",
             model="llama3:8b"
         )
